46cd02b661bc508c7482dc9c267c998eeea861fe
Tanner Mickelson <tanner.mickelson@hidesigns.co.jp>
2017-01-25 11:48:51 -0800

    Started on line selection.

--- RenderingSystem/source/RenderingSystem.cpp
+++ RenderingSystem/source/RenderingSystem.cpp
@@ -1,3 +1,5 @@
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include "LotsOfLines/RenderingSystem.hpp"
 #include "LotsOfLines/IRenderer.hpp"
 
@@ -64,7 +66,9 @@ void RenderingSystem::setVisualizationType(E_VISUALIZATION_TYPE type)
 		//Generate VBO if needed
 		if (m_vboCache.find(type) == m_vboCache.end())
 		{
-			m_vboCache[type] = generateFromDataSet(m_dataSet, type);
+			std::vector<Vertex> vertices;
+			m_vboCache[type] = generateFromDataSet(m_dataSet, type, vertices);
+			m_vertices[type] = vertices;
 		}
 	}
 }
@@ -92,7 +96,7 @@ void RenderingSystem::drawVisualization()
 	}
 }
 
-std::shared_ptr<IVertexBufferObject> RenderingSystem::generateFromDataSet(std::shared_ptr<DataSet> dataSet, E_VISUALIZATION_TYPE type)
+std::shared_ptr<IVertexBufferObject> RenderingSystem::generateFromDataSet(std::shared_ptr<DataSet> dataSet, E_VISUALIZATION_TYPE type, std::vector<Vertex>& verticesOut)
 {
 	auto iter = m_visualizationMethods.find(type);
 	if (iter == m_visualizationMethods.end())
@@ -102,11 +106,11 @@ std::shared_ptr<IVertexBufferObject> RenderingSystem::generateFromDataSet(std::s
 
 	std::shared_ptr<IVisualizationMethod> method = iter->second;
 
-	std::vector<Vertex> vertices;
+	std::vector<Vertex> verticesOut;
 	std::vector<unsigned int> indices;
-	if (method->generateVBO(dataSet, vertices, indices))
+	if (method->generateVBO(dataSet, verticesOut, indices))
 	{
-		return m_driver->createVBO(vertices, indices);
+		return m_driver->createVBO(verticesOut, indices);
 	}
 
 	return nullptr;
@@ -115,6 +119,25 @@ std::shared_ptr<IVertexBufferObject> RenderingSystem::generateFromDataSet(std::s
 void RenderingSystem::drawVBO(std::shared_ptr<IVertexBufferObject> vbo)
 {
 	m_driver->drawVBO(vbo);
+}
+
+unsigned int RenderingSystem::getClosestLine(float x, float y)
+{
+	float closestDist = std::numeric_limits<float>::max();
+	unsigned int closestLine = 0;
+	for (auto vertex : m_vertices[m_currentVisualizationType])
+	{
+		glm::vec2 m(x, y);
+		glm::vec2 v(vertex.x, vertex.y);
+		float dist = glm::distance(m, v);
+		if (dist < closestDist)
+		{
+			closestDist = dist;
+			closestLine = vertex.lineIndex;
+		}
+	}
+
+	return closestLine;
 }
 
 // TEMPORARY FOR GHETTO TESTING PURPOSES. TESTS WILL BE MOVED TO THE TEST FRAMEWORK SHORTLY.
@@ -135,7 +158,7 @@ int main()
 	renderer.setDataSet(data);
 
 	renderer.setViewTransform(0.0f, 3.7f, 1.0f, 0.2f);
-	renderer.setVisualizationType(EVT_PARALLEL_COORDINATES);
+	renderer.setVisualizationType(EVT_COLLOCATED_PAIRED_COORDINATES);
 
 	//renderer.setViewTransform(0.0f, 0.2f, 1.0f, 1.0f);
 
