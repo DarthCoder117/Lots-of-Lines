abff6e302639cac2bbc860307fc18f70fe46f228
Tanner Mickelson <tanner.mickelson@hidesigns.co.jp>
2017-03-01 20:09:33 -0800

    Got Lots of Lines launching from Excel. Working on command line parameters.

--- Application/LotsOfLinesApp/LotsOfLinesApp/main.cpp
+++ Application/LotsOfLinesApp/LotsOfLinesApp/main.cpp
@@ -1,10 +1,18 @@
 #include "LotsOfLinesApp.h"
 #include <QtWidgets/QApplication>
+#include <QCommandLineParser>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    LotsOfLinesApp w;
+    QApplication app(argc, argv);
+	app.setApplicationName("Lots of Lines");
+
+	QCommandLineParser parser;
+	parser.addPositionalArgument("file", "Data file to open");
+
+	const QStringList args = parser.positionalArguments();
+
+    LotsOfLinesApp w(!args.isEmpty() ? args.at(0) : "");
     w.show();
-    return a.exec();
+    return app.exec();
 }
--- ExcelAddin/LotsOfLinesExcel/LotsOfLinesExcel/LotsOfLinesRibbon.cs
+++ ExcelAddin/LotsOfLinesExcel/LotsOfLinesExcel/LotsOfLinesRibbon.cs
@@ -6,6 +6,8 @@ using Microsoft.Office.Tools.Ribbon;
 using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 using Microsoft.Office.Tools.Excel;
+using System.Windows.Forms;
+using System.IO;
 
 namespace LotsOfLinesExcel
 {
@@ -16,13 +18,44 @@ namespace LotsOfLinesExcel
             this.openInLotsOfLinesButton.Click += OpenInLotsOfLinesButton_Click;
         }
 
+        private Dictionary<int, string> tempFileMap = new Dictionary<int, string>();
+
         private void OpenInLotsOfLinesButton_Click(object sender, RibbonControlEventArgs e)
         {
+            //Create temp CSV file
+            string tempPath = System.IO.Path.GetTempPath();
+            string tempFile = tempPath + Guid.NewGuid().ToString() + ".csv";
+            File.Create(tempFile);
+            Globals.ThisAddIn.Application.ActiveWorkbook.SaveCopyAs(tempFile);
+
+            //Open Lots of Lines application
             string lotsOfLinesDir = Environment.GetEnvironmentVariable("LOTS_OF_LINES_PATH");
             string lotsOfLinesFilename = lotsOfLinesDir + "\\LotsOfLinesApp.exe";
-            System.Diagnostics.Process.Start(lotsOfLinesFilename);
+            try
+            {
+                System.Diagnostics.Process proc = System.Diagnostics.Process.Start(lotsOfLinesFilename, tempFile);
+                tempFileMap[proc.Id] = tempFile;
+                proc.Exited += Proc_Exited;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error loading Lots of Lines");
+            }
 
             Excel.Workbook workbook = Globals.ThisAddIn.Application.ActiveWorkbook;
+        }
+
+        private void Proc_Exited(object sender, EventArgs e)
+        {
+            string tempFile = tempFileMap[((System.Diagnostics.Process)sender).Id];
+            try
+            {
+                File.Delete(tempFile);
+            }
+            catch (Exception ex)
+            {
+                Console.Out.WriteLine(ex.Message);
+            }
         }
     }
 }
